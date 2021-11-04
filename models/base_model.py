b83ecc41b0c5e7eb5f268394d614abafad067cc5
Nel10 <3461@holbertonschool.com>
2021-11-04 17:24:16 -0500

    update class

--- models/base_model.py
+++ models/base_model.py
@@ -1,41 +1,39 @@
 #!/usr/bin/python3
-"""Class base"""
+"""
+Class base
+"""
 
 from datetime import datetime
-from uuid import uuid4
 import models
 import uuid
-import json
-format_datetime = "%Y-%m-%dT%H:%M:%S.%f"
+ft_dt = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """"""
+    """BaseModel class"""
     def __init__(self, *args, **kwargs):
-        """"""
-        if args is not None and len(args) > 0:
-            pass
+        """initialization of the class"""
         if kwargs:
-            for key, item in kwargs.items():
+            for key, value in kwargs.items():
                 if key in ['created_at', 'updated_at']:
-                    item = datetime.strptime(item, format_datetime)
+                    value = datetime.strptime(value, ft_dt)
                 if key not in ['__class__']:
-                    setattr(self, key, item)
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """"""
+        """string representation"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
                                      self.__dict__)
 
     def save(self):
-        """"""
+        """update the current date"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """"""
+        """returns the contents of the dict"""
         dic_t = {}
         for key, item in self.__dict__.items():
             if key in ['created_at', 'updated_at']:
