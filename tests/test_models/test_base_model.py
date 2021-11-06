4e7c40858605ad629de65a4930cf1e2f2d52a768
Nel10 <3461@holbertonschool.com>
2021-11-06 13:07:25 -0500

    teste basemodel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,8 +5,16 @@ import os
 from models.base_model import BaseModel
 import pep8 as pycodestyle
 
+
 class TestBaseModel(unittest.TestCase):
     """Test BaseModel"""
 
     def setUpClass(self):
+        """set up class"""
+        self.test_basemodel = BaseModel()
 
+    def test_pep8_BaseModel(self):
+        """test with PEP8"""
+        pycstyle = pep8.StyleGuide(quiet=True)
+        style = pycstyle.check_file(['models/base_model.py'])
+        self.assertEqual(style.total_errors, 0, "Check pep8")
