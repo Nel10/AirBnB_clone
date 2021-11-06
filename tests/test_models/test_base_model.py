184c00fa3d039d410dd8ba43c8cca8fee0d789f4
Nel10 <3461@holbertonschool.com>
2021-11-06 12:52:17 -0500

    update test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Test BaseModel"""
+import unittest
+import os
+from models.base_model import BaseModel
+import pep8 as pycodestyle
+
+class TestBaseModel(unittest.TestCase):
+    """Test BaseModel"""
+
+    def setUpClass(self):
+
