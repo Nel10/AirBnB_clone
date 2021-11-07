bd85c8a638620cd11f53cdc41d5a9f01b04fce98
Nel10 <3461@holbertonschool.com>
2021-11-07 18:43:56 -0500

    console

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""inicializer console"""
+import cmd
+import datetime
+from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    """contains the entry point of the command interpreter"""
+    prompt = '(hbnb) '
+
+    def do_EOF(self, arg):
+        """End Of File"""
+        return True
+
+    def do_quit(self, arg):
+        """exit the program"""
+        return True
+
+    def emptyline(self):
+        """dont execute nothing"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
