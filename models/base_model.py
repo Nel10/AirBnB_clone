73dae294e81885a105c49bfe6a15fef074506f54
Nel10 <3461@holbertonschool.com>
2021-11-07 16:49:53 -0500

    update

--- models/base_model.py
+++ models/base_model.py
@@ -25,6 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """string representation"""
