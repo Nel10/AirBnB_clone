64c0c1d797febf950d28a43be296d2596b07d8d0
Nel10 <3461@holbertonschool.com>
2021-11-09 12:39:39 -0500

    user.py

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class User"""
+
+
+class User(BaseModel):
+    """class User that inherits from BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
